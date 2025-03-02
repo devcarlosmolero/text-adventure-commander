b254cfebb62ef76051ff181d500ff8c32953ef4a
devcarlosmolero <dev@carlosmolero.com>
2025-03-02 23:07:11 +0100

    fixed execute_command & added print_available_commands method

--- example/levels/level_1.rb
+++ example/levels/level_1.rb
@@ -1,36 +1,35 @@
 require "text-engine"
 
 class Level1
-  ALLOWED_SYMBOLS = ["GO", "RIGHT", "LEFT", "TOP", "BOTTOM"]
   COMMANDS = {
-    GO_RIGHT: :Right,
-    GO_LEFT: :Left,
-    GO_TOP: :Top,
-    GO_BOTTOM: :Bottom
+    GO_RIGHT: :go_right,
+    GO_LEFT: :go_left,
+    GO_TOP: :go_top,
+    GO_BOTTOM: :go_bottom
   }
   def self.Start
-    text_engine = TextEngine.new(ALLOWED_SYMBOLS, COMMANDS)
-    puts "Where do you want to go?"
+    text_engine = TextEngine.new(COMMANDS)
+    puts "Where do you want to go? #{text_engine.print_available_commands}"
     input = gets
     result = text_engine.get_commands_from(input)
     result[:recognized].each do |command|
-      send(COMMANDS[command.tr(" ", "_").upcase.to_sym])
+      text_engine.execute_command(Level1, command)
     end
   end
 
-  def self.Right
+  def self.go_right
     puts "You went to the right"
   end
 
-  def self.Left
+  def self.go_left
     puts "You went to the left"
   end
 
-  def self.Top
+  def self.go_top
     puts "You went to the top"
   end
 
-  def self.Bottom
+  def self.go_bottom
     puts "You went to the bottom"
   end
 end
